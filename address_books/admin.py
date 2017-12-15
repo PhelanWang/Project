02a1bc29d7fb99b96f2574c45a6cbada9ba84426
phelan <1825633959@qq.com>
2017-12-15 18:13:34 +0800

    add .gitignore

---
+++ address_books/admin.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+
+from address_books.models import Category, Entry
+
+# admin.site.register(Category, admin_class=None)
+# admin.site.register(Entry, admin_class=None)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    pass
